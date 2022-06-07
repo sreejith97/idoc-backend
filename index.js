09d8a47522884b16ed6386c6e59cf0e107fb7923
Sreejith <sreejithsp711@gmail.com>
2022-06-07 23:15:01 +0530

    test

--- index.js
+++ index.js
@@ -55,6 +55,10 @@ app.get("/testone", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(` Listen at PORT : ${PORT}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
